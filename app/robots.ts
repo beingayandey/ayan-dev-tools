108cefc66eac3c4b03cf841348a893e6a1684f7f
ayancofinex <ayandey166@gmail.com>
2026-04-03 14:33:22 +0530

    chore: update sitemap URL in robots.ts to point to the correct production domain

--- app/robots.ts
+++ app/robots.ts
@@ -9,6 +9,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/private/',
     },
-    sitemap: 'https://beingayandey.me/tools/sitemap.xml', // Replace with your domain
+    sitemap: 'https://ayan-dev-tools.vercel.app/sitemap.xml',
   };
 }
